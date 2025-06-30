032bc9574c5845d1073737687d32d76613ee3cdb
Andrei Mazhyrau <andreymozhirov@gmail.com>
2025-06-30 22:29:31 +0200

    Fix build on Mac with Apple Silicon

--- unit-test.sh
+++ unit-test.sh
@@ -5,8 +5,17 @@
 # Create a build subdirectory, change into it, run
 # cmake .. && make && run the assignment-autotest application
 mkdir -p build
-cd build
-cmake ..
+cd build || (echo "Error: Unable change directory to ./build" && exit 1)
+
+# Check if we're on macOS with Apple Silicon
+if [ "$(uname)" = "Darwin" ] && [ "$(uname -m)" = "arm64" ]; then
+    # On Mac with Apple Silicon, use x86_64 toolchain
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=../x86_64-toolchain.cmake
+else
+    # otherwise, use default toolchain
+    cmake ..
+fi
+
 make clean
 make
 cd ..
